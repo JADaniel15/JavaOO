b68a7a58a6421f135b9f72a5a9098129f7e7f5d3
João Augusto <j.asoul@hotmail.com>
2022-07-01 00:13:28 -0300

    novas pasta e novo encapsulamento

--- bytebank-encapsulado/src/conta.java
+++ bytebank-encapsulado/src/conta.java
@@ -1,8 +1,14 @@
 class conta{
 	private double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	
+	public conta(int agencia, int numero) {
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("criando uma nova conta "  +  this.numero);
+	}
 	
 	 public void deposita(double valor) {
 		this.saldo += valor;
@@ -28,4 +34,32 @@ class conta{
 	public double getSaldo() {
 		return this.saldo;
 	}
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero){
+		if(numero <=0) {
+			System.out.println("Não pode valor menor <= 0 ");
+			return;
+		}
+		this.numero = numero;
+	}
+	public int getAgencia() {
+		return this.agencia;
+	}
+	public void setAgencia(int agencia) {
+		if(agencia <= 0) {
+			System.out.println("Não pode valor menor a 0 ");
+			return;
+		}
+		this.agencia = agencia;
+	}
+	public Cliente getTitular() {
+		return titular;
+	}
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
 }
