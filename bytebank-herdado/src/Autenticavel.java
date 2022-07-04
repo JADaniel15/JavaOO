decf7a5a71e9856bfc671794b49b1951c6d59631
João Augusto <j.asoul@hotmail.com>
2022-07-03 22:48:54 -0300

    nova classes eimplemtencao de interfaces

---
+++ bytebank-herdado/src/Autenticavel.java
@@ -0,0 +1,13 @@
+
+// contarto autenticavel
+	// quem assina o contrato, implemetar
+		// metdo setSenha
+		// metedo autenticavel
+public abstract interface Autenticavel  {
+	
+	public abstract void setSenha(int senha);
+	
+	public abstract boolean autentica(int senha);
+		
+
+}
