2aa441ba35c79b036bd3ecb23c314478f864f33d
João Augusto <j.asoul@hotmail.com>
2022-07-04 22:26:47 -0300

    curso de excercao

--- java-pilha/src/FluxoComErro.java
+++ java-pilha/src/FluxoComErro.java
@@ -1,10 +1,10 @@
 
-public class Fluxo {
+public class FluxoComErro {
 	public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
         	 metodo1();
-        } catch(Exception ex) {
+        } catch(ArithmeticException | NullPointerException ex) {
         	String msg = ex.getMessage();
         	System.out.println("Exception " + msg);
         	ex.printStackTrace();
@@ -13,16 +13,15 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1()throws MinhaExcecao {
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao {
-        System.out.println("Ini do metodo2");  
-        throw new MinhaExcecao("deu  muito errado");
-        
-        //System.out.println("Fim do metodo2");
+    private static void metodo2() {
+       System.out.println("ini metodo 2");
+       metodo2();
+       System.out.println("fim metodo 2");
     }
 }
