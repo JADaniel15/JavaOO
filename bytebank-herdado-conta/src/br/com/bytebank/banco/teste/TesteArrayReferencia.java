342289617919cb1f3c920f634b450b049f2f2066
João Augusto <j.asoul@hotmail.com>
2022-07-06 23:14:33 -0300

    novas classes

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencia.java
@@ -0,0 +1,38 @@
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Object;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
+public class TesteArrayReferencia {
+
+	public static void main(String[] args) {
+		
+		Object[] referencia= new Object[5];
+		
+		
+		ContaCorrente cc1 = new ContaCorrente(22, 11);
+		referencia[0] =cc1;
+		
+		ContaPoupanca cc2 = new  ContaPoupanca(22, 22);
+		referencia[1] =cc2;
+		
+		//Cliente cliente = new  Cliente();
+		//referencia[2] = cliente;
+		
+		
+		//System.out.println(cc2.getNumero());
+		
+		//Object referenciaGenerica = contas[1];
+		
+		//System.out.println(referenciaGenerica.getNumero());
+		
+		ContaPoupanca ref =(ContaPoupanca) referencia[1];
+		System.out.println(cc2.getNumero());
+		System.out.println(ref.getNumero());
+		
+		
+	}
+
+}
---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteGuardadorReferencias.java
@@ -0,0 +1,25 @@
+package br.com.bytebank.banco.teste;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.GuardadorDeReferencias;
+
+public class TesteGuardadorReferencias {
+
+        public static void main(String[] args) {
+
+            GuardadorDeReferencias guardador = new GuardadorDeReferencias();
+
+            ContaCorrente cc = new ContaCorrente(22, 11);
+            guardador.adiciona(cc);
+
+            ContaCorrente cc2 = new ContaCorrente(22, 22);
+            guardador.adiciona(cc2);
+
+            int tamanho = guardador.getQuantidadeDeElementos();
+            System.out.println(tamanho);
+
+            ContaCorrente ref = (ContaCorrente) guardador.getReferencia(1);
+            System.out.println(ref.getNumero());
+        }
+
+}
